9de70d1627e58830f3b33ad967be0b9a0d6d2ac6
Justin Kulikauskas <jkulikau@redhat.com>
2024-08-13 17:04:02 -0400

    open a build-a-bundle workshop

---
+++ hack/scripts/integration-setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if which yq; then
+    echo "Using pre-installed yq"
+else
+    echo "Installing yq"
+    YQ_VERSION=v4.44.3
+    YQ_BINARY=yq_linux_amd64
+    wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz -O - |\
+    tar xz && mv ${YQ_BINARY} /usr/bin/yq
+    which yq
+fi
+
+echo "${SNAPSHOT}" > snapshot.json
+
+csvPath='../../bundle/manifests/multiclusterhub-operator.clusterserviceversion.yaml'
+containerEnvPath='.spec.install.spec.deployments[0].spec.template.spec.containers[0].env'
+imgCfgPath='/tmp/vol/image-config.yaml'
+
+yq -i '.spec.relatedImages = []' "${csvPath}"
+
+for kebabName in $(yq -o=yaml 'keys | .[]' "${imgCfgPath}"); do
+    snakeName="${kebabName//-/_}"
+    bigName=$(echo "${snakeName}" | tr "[:lower:]" "[:upper:]")
+    snapName="${kebabName}-acm-211"
+
+    image=$(yq '.["'"${kebabName}"'"]' "${imgCfgPath}")
+    if yq -e -o=yaml '.components[] | select(.name == "'"${snapName}"'").containerImage' snapshot.json > /dev/null 2>&1; then
+        echo "Using image from snapshot for ${kebabName}"
+        image=$(yq -o=yaml '.components[] | select(.name == "'"${snapName}"'").containerImage' snapshot.json)
+    else
+        echo "Using default image for ${kebabName}"
+    fi
+
+    newEnv="{\"name\": \"OPERAND_IMAGE_${bigName}\", \"value\": \"${image}\"}"
+    yq -i "${containerEnvPath} += ${newEnv}" "${csvPath}"
+
+    newRelatedImg="{\"name\": \"${snakeName}\", \"image\": \"${image}\"}"
+    yq -i ".spec.relatedImages += ${newRelatedImg}" "${csvPath}"
+done
+
+cat "${csvPath}"
+
+echo "... now what?"
